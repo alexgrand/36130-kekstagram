b947219249e23a7080251143d299222f18a420c5
Alexrand <namelost@yandex.ru>
2018-02-02 17:34:26 +0800

    added: funcs for random generatng of photos;

--- js/pictures.js
+++ js/pictures.js
@@ -1,1 +1,37 @@
 'use strict';
+var NUMBER_OF_PICTURES = 25;
+var ALL_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var MAX_LIKES = 200;
+var MIN_LIKES = 15;
+var MAX_COMMENTS = 2;
+var pictures = [];
+
+var getRandomNumber = function (max, min) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var getCommentsArray = function (numOfComments) {
+  var comments = [];
+  for (var i = 0; i < numOfComments; i++) {
+    var index = getRandomNumber(ALL_COMMENTS.length - 1, 0);
+    comments[i] = ALL_COMMENTS[index];
+  }
+  return comments;
+};
+
+var createPictureoObject = function (index) {
+  return {
+    url: 'photos/' + index + '.jpg',
+    likes: getRandomNumber(MAX_LIKES, MIN_LIKES),
+    comments: getCommentsArray(getRandomNumber(MAX_COMMENTS, 1))
+  };
+};
+
+var generateAllPictures = function () {
+  for (var i = 0; i < NUMBER_OF_PICTURES; i++) {
+    pictures[i] = createPictureoObject(i + 1);
+  }
+};
+
+generateAllPictures();
+
