6cf106f68e14296b20e86fa3855c37aa0f9a5e50
Alexrand <namelost@yandex.ru>
2018-02-13 21:15:14 +0700

    added IIFE for uploadForm;

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,4 @@
-'use sctrict';
+'use strict';
 (function () {
   var picturesElement = document.querySelector('.pictures');
 
--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,4 @@
 'use strict';
-
 (function () {
   var effectsContainerElement = document.querySelector('.upload-effect__container');
   var effectControlsElement = effectsContainerElement.querySelector('.upload-effect-controls');
@@ -189,50 +188,53 @@
   };
 })();
 
-var uploadFileElement = document.querySelector('#upload-file');
-var uploadOverlay = document.querySelector('.upload-overlay');
-var uploadFormCancelElement = uploadOverlay.querySelector('.upload-form-cancel');
+(function () {
+  var uploadFileElement = document.querySelector('#upload-file');
+  var uploadOverlay = document.querySelector('.upload-overlay');
+  var uploadFormCancelElement = uploadOverlay.querySelector('.upload-form-cancel');
 
-var openUploadOverlay = function () {
-  uploadOverlay.classList.remove('hidden');
-  window.effects.addEffectsHandlers();
-  window.validation.addValidationHandlers();
-  window.utils.runHandlers(openCloseUploadHandlers, true);
-};
-var closeUploadOverlay = function () {
-  uploadOverlay.classList.add('hidden');
-  uploadFileElement.value = '';
-  window.effects.removeEffectsHandlers();
-  window.validation.removeValidationHandlers();
-  window.utils.runHandlers(openCloseUploadHandlers, false);
-};
+  var openUploadOverlay = function () {
+    uploadOverlay.classList.remove('hidden');
+    window.effects.addEffectsHandlers();
+    window.validation.addValidationHandlers();
+    window.utils.runHandlers(openCloseUploadHandlers, true);
+  };
+  var closeUploadOverlay = function () {
+    uploadOverlay.classList.add('hidden');
+    uploadFileElement.value = '';
+    window.effects.removeEffectsHandlers();
+    window.validation.removeValidationHandlers();
+    window.utils.runHandlers(openCloseUploadHandlers, false);
+  };
 
-var onUploadFileChange = function () {
-  openUploadOverlay();
-};
-var onUploadFormEscPress = function (evt) {
-  window.utils.onDocumentEscPress(evt, closeUploadOverlay);
-};
-var onUploadFormCancelClick = function () {
-  closeUploadOverlay();
-};
+  var onUploadFileChange = function () {
+    openUploadOverlay();
+  };
+  var onUploadFormEscPress = function (evt) {
+    window.utils.onDocumentEscPress(evt, closeUploadOverlay);
+  };
+  var onUploadFormCancelClick = function () {
+    closeUploadOverlay();
+  };
 
-var uploadHandlers = [
-  {element: uploadFileElement,
-    eventType: 'change',
-    handler: onUploadFileChange
-  }
-];
+  var uploadHandlers = [
+    {element: uploadFileElement,
+      eventType: 'change',
+      handler: onUploadFileChange
+    }
+  ];
 
-var openCloseUploadHandlers = [
-  {element: uploadFormCancelElement,
-    eventType: 'click',
-    handler: onUploadFormCancelClick
-  },
-  {element: document,
-    eventType: 'keydown',
-    handler: onUploadFormEscPress
-  }
-];
+  var openCloseUploadHandlers = [
+    {element: uploadFormCancelElement,
+      eventType: 'click',
+      handler: onUploadFormCancelClick
+    },
+    {element: document,
+      eventType: 'keydown',
+      handler: onUploadFormEscPress
+    }
+  ];
 
-window.utils.runHandlers(uploadHandlers, true);
+  window.utils.runHandlers(uploadHandlers, true);
+})();
+
